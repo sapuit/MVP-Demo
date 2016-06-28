d72b80352f763010ef4ed7707e7a9c5895b36757
sapuit <sapnkp@gmail.com>
2016-06-28 16:44:32 +0700

    first commit

---
+++ app/src/main/java/vn/soapp/mvpdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package vn.soapp.mvpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
